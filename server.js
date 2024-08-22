b585f03159cd037e72cbd96a43731d8405cb54cc
Vishaldeep3000 <vsdviroli414@gmail.com>
2024-08-22 16:17:34 +0530

    Updated the port

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //as we know we are sendin data in JSON format from postMan
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 
+//Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`${new Date().toLocaleString} request made to : ${req.originalUrl}`);
+    next();
+}
+
 //stating URL
-app.get('/', function(req, res){
+app.get('/', logRequest, function(req, res){
     res.send("Hey I am your waiter, how can i help you ?");
 })
 
